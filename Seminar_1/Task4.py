b83d54dcdff3e9d730d23831183598f8b35c4cc6
Anna <shtefanova.1996@mail.ru>
2024-02-13 21:56:29 +0300

    Solved Task4

--- Seminar_1/Task4.py
+++ Seminar_1/Task4.py
@@ -9,7 +9,7 @@ Output: YES
 '''
 year = int (input('Введите год: '))
 
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
     print("YES")
 else:
     print("NO")
