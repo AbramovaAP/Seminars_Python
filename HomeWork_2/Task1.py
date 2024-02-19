1a9384a8cfc08efac9f1adc4d4fdf63ab801986f
Anna <shtefanova.1996@mail.ru>
2024-02-19 22:22:42 +0300

    Solved Task1

--- HomeWork_2/Task1.py
+++ HomeWork_2/Task1.py
@@ -8,3 +8,17 @@
 5 -> 1 0 1 1 0
 2
 '''
+coins = [0, 0, 0, 1, 0]
+count_zero = 0
+count_one = 0
+
+for coin in coins:
+    if coin == 0:
+        count_zero += 1
+    else:
+        count_one += 1
+    
+if count_one > count_zero:
+    print(count_zero)
+else:
+    print(count_one)
