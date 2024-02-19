68b929e1479604a8c34944b031517b50fdf4aa27
Anna <shtefanova.1996@mail.ru>
2024-02-19 14:45:13 +0300

    Solved Task1

--- Seminar_2/Task1.py
+++ Seminar_2/Task1.py
@@ -7,3 +7,20 @@ while
 Input: 5
 Output: 120    
 '''
+# Решение 1:
+num = int (input('Введите число, для получения факториала: '))
+factorial = 1
+count = 1
+while (count <= num):
+    factorial *= count
+    count += 1
+
+print (factorial)
+
+#Решение 2:
+num = int (input('Введите число, для получения факториала: '))
+factorial = 1
+while (num > 1):
+    factorial *= num
+    num -= 1
+print (factorial)
