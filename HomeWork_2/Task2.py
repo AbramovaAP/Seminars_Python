a2ba87de0cc37512aef7d8cbf18977496d5e78b1
Anna <shtefanova.1996@mail.ru>
2024-02-19 22:55:30 +0300

    Solved Task2

--- HomeWork_2/Task2.py
+++ HomeWork_2/Task2.py
@@ -12,17 +12,6 @@
 s = 5
 p = 6
 
-# solutions = []
-# for i in range(1, 1001):
-#     for j in range(1, 1001):
-#         if s == i + j and p == i * j:
-#             solutions.append((min(i, j), max(i, j)))
-#             solutions = list(set(solutions))
-
-# for solution in solutions:
-#     print(solution[0], solution[1])
-
-
 numbers = [] # пустой массив для двух чисел
 
 for x in range(1, 1001):
