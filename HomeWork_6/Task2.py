16a9ad56f21fb90a5331563f8a42873144d75662
Anna <shtefanova.1996@mail.ru>
2024-03-02 02:44:32 +0300

    Solved Task2

--- HomeWork_6/Task2.py
+++ HomeWork_6/Task2.py
@@ -8,3 +8,13 @@
     0, -5, -5, 7]
     Вывод: [1, 9, 13, 14, 19]
 '''
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+
+min_number = 0
+max_number = 10
+
+for i in range(0, len(list_1)):
+    if min_number < list_1[i] <= max_number:
+        #print(i, "-", list_1[i])
+        print(i)
